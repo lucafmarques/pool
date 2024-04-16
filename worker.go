b552bc6cc8b7719a0133466535b441a1a1b38c3d
lucafmarques <lucafmarqs@gmail.com>
2024-04-15 23:49:51 -0300

    feat: Add early API

--- worker.go
+++ worker.go
@@ -1,1 +1,27 @@
 package pool
+
+import (
+	"context"
+	"sync"
+)
+
+func worker(ctx context.Context, wg *sync.WaitGroup, first func(), tasks <-chan func(), executor func(func(), bool)) {
+	defer wg.Done()
+
+	if first != nil {
+		executor(first, true)
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case task, ok := <-tasks:
+			if task == nil || !ok {
+				return
+			}
+
+			executor(task, false)
+		}
+	}
+}
